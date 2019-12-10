894409f7e49ee2834e41564e548ebcecb6ca1f68
Uriah <uriah.shm@outlook.com>
2019-12-10 21:45:10 +0200

    final touches

--- calculator.cpp
+++ calculator.cpp
@@ -12,7 +12,8 @@ using namespace std;
 #define MATH_FUNCTION(name,statements) inline double name (double a, double b) { \
 statements \
 }
-#define CONST(name,value) pair<string, double>{ pair<string, double>(name,value)},
+#define EQUATION_IS_EMPTY_ID 0
+#define NOT_A_NUMBER_NOT_A_CONST_ID 1
 
 MATH_FUNCTION(_add, return a + b;)
 MATH_FUNCTION(_sub, return a - b;)
@@ -20,7 +21,24 @@ MATH_FUNCTION(_mul, return a * b;)
 MATH_FUNCTION(_div, return a / b;)
 MATH_FUNCTION(_pow, return pow(a, b);)
 
-const map<char, double (*)(double, double)> ACTION_TO_FUNCTION_MAP = map<char, double (*)(double, double)>({
+
+class calculator_exeption : invalid_argument {
+public:
+	int id;
+	calculator_exeption(int);
+	string get_message() {
+		if (this->id == EQUATION_IS_EMPTY_ID)
+			return "Equation is empty!";
+		else if (this->id == NOT_A_NUMBER_NOT_A_CONST_ID)
+			return "Not a number nor a const!";
+	}
+};
+calculator_exeption::calculator_exeption(int id) :invalid_argument("Syntax Error") { //Initializer list must be used 
+	this->id = id;
+}
+
+const map<char, double (*)(double, double)> ACTION_TO_FUNCTION_MAP = map<char, double (*)(double, double)>(
+	{
 		{'+', _add},
 		{'-',_sub},
 		{'*',_mul},
@@ -28,7 +46,9 @@ const map<char, double (*)(double, double)> ACTION_TO_FUNCTION_MAP = map<char, d
 		{'^',_pow}
 	}
 );
-const map<char, int> ACTION_TO_PRIORITY_MAP = map<char, int>({
+
+const map<char, int> ACTION_TO_PRIORITY_MAP = map<char, int>(
+	{
 		{'+', 5},
 		{'-',5},
 		{'*',4},
@@ -37,43 +57,25 @@ const map<char, int> ACTION_TO_PRIORITY_MAP = map<char, int>({
 	}
 );
 
-pair<string, double> CONSTANTS[] = {
-		CONST("e",M_E)
-};
+const map<string, double> CONSTANTS = map<string, double>(
+	{
+		{"e",M_E},
+		{"pi",M_PI},
+	}
+);
+
+inline string my_substr(const string & str, unsigned int idx_start, unsigned int idx_end) {
+	return str.substr(idx_start, idx_end - idx_start);
+}
 
 inline bool is_digit(char c) {
 	return c <= '9' && c >= '0';
 }
 
-inline bool is_bracket(char c) {
-	return c == '(' || c == ')';
-}
-
-unsigned int get_idx_of_last_char(DEFAULT_ARGS, char c) {
-	for (unsigned int i = idx_end - 1; i >= idx_start; i--) {
-		if (i == -1) return idx_start;
-
-		if (s[i] == c)
-			return i + 1;
-	}
-	return idx_start;
-}
-
-unsigned int get_idx_of_first_char(DEFAULT_ARGS, char c) {
-	for (unsigned int i = idx_start; i < idx_end; i++) {
-		if (s[i] == c)
-			return i - 1;
-	}
-	return idx_end;
-}
-
-
 bool is_number(DEFAULT_ARGS) {
-	if (idx_start == idx_end)
-		return 0;
 	bool dot_flag = false;
 	for (int i = idx_start; i < idx_end; i++) {
-		if (!is_digit(s[i]) && !is_bracket(s[i]))
+		if (!is_digit(s[i]))
 			if (s[i] == '.' && !dot_flag) {
 				dot_flag = true;
 			}
@@ -85,28 +87,16 @@ bool is_number(DEFAULT_ARGS) {
 }
 
 double parse_constant(const string & s) {
-	string actual_s = string(s);
-
-	actual_s.erase(std::remove(actual_s.begin(), actual_s.end(), '('), actual_s.end());
-	actual_s.erase(std::remove(actual_s.begin(), actual_s.end(), ')'), actual_s.end());
-	for (const auto& pr : ACTION_TO_FUNCTION_MAP) {
-		actual_s.erase(std::remove(actual_s.begin(), actual_s.end(), pr.first), actual_s.end());
-	}
-
-
-	for (int i = 0; i < size(CONSTANTS); i++) {
-		if (actual_s == CONSTANTS[i].first)
-			return CONSTANTS[i].second;
-	}
+	if (CONSTANTS.count(s))
+		return CONSTANTS.at(s);
 	return NAN;
 }
 
-
 int find_action_idx(DEFAULT_ARGS) {
-	unsigned int tmp_idx_low = idx_start, tmp_level_low = -1, stk = 0, priority = 1;
+	unsigned int tmp_idx_low = 0, tmp_level_low = -1, stk = 0, priority = 1;
 	char tmp_char = 0;
-	for (unsigned int i = idx_end - 1; i >= idx_start; i--) {
-		if (i == -1)
+	for (int i = idx_end - 1; i >= idx_start; i--) {
+		if (i == idx_start - 1)
 			break;
 		tmp_char = s[i];
 		if (tmp_char == ')') stk++;
@@ -138,53 +128,85 @@ bool str_contains_action(DEFAULT_ARGS) {
 	return false;
 }
 
+bool remove_redundent_brackets(DEFAULT_ARGS) {
+	signed int stk = 1;
+	bool not_flag = false;
+	char tmp_char = 0;
+	for (unsigned int i = idx_start + 1; i < idx_end; i++) {
+		tmp_char = s[i];
+		if (tmp_char == ')') stk--;
+		else if (tmp_char == '(') stk++;
+		if (stk == 0 && i != idx_end - 1) {
+			return false;
+		}
+	}
+	return (stk == 0 && s[idx_end - 1] == ')') || not_flag;
+}
+
 
 double solve_eq(DEFAULT_ARGS) {
-	unsigned int idx_of_first_char = get_idx_of_first_char(s, idx_start, idx_end, ')');
-	unsigned int idx_of_last_char = get_idx_of_last_char(s, idx_start, idx_end, '(');
+	if (idx_start == idx_end)
+		throw calculator_exeption(EQUATION_IS_EMPTY_ID);
 
+	while (s[idx_start] == '(') {
+		if (remove_redundent_brackets(s, idx_start, idx_end)) {
+			idx_start++;
+			idx_end--;
+		}
+		else break;
+	}
 	if (str_contains_action(s, idx_start, idx_end)) {
-		unsigned int action_idx = find_action_idx(s, idx_start, idx_end);
-		char c = s[action_idx];
-		return ACTION_TO_FUNCTION_MAP.at(c)(solve_eq(s, idx_start, action_idx), solve_eq(s, action_idx + 1, idx_end));
+		const unsigned int action_idx = find_action_idx(s, idx_start, idx_end);
+		const char c = s[action_idx];
+		double a, b;
+		try {
+			a = solve_eq(s, idx_start, action_idx);
+		}
+		catch (calculator_exeption e) {
+			if (e.id == EQUATION_IS_EMPTY_ID && c == '-')
+				a = 0;
+			else
+				throw e;
+		}
+		b = solve_eq(s, action_idx + 1, idx_end);
+		double (*f)(double, double) = ACTION_TO_FUNCTION_MAP.at(c);
+		return f(a, b);
 	}
 	else {
 		if (is_number(s, idx_start, idx_end)) {
-			return stod(s.substr(idx_of_last_char, idx_of_first_char));
+			return stod(my_substr(s, idx_start, idx_end));
 		}
-		else {/* TODO - fix constants
-			double _const = parse_constant(s.substr(idx_of_last_char, idx_of_first_char));
+		else {
+			double _const = parse_constant(my_substr(s, idx_start, idx_end));
 			if (!isnan(_const)) {
 				return _const;
 			}
-			*/
 		}
+		throw calculator_exeption(NOT_A_NUMBER_NOT_A_CONST_ID);
 	}
 }
 
 
-double solve_eq(string & s) {
-	try {
-		return solve_eq(s, 0, s.length());
-	}
-	catch (exception e) {
-		throw e;
-	}
-}
 
 
 int main()
 {
+	cout << "Advanced Calculator - Uriah Shaul Mandel" << endl;
+	cout << "This calculator follows order of arithmetic operations" << endl;
+	cout << "The operetors supported: +, -, *, /, ^, ()" << endl;
+	cout << "The consts supported: e, pi" << endl;
+	cout << "Have fun :)" << endl << endl << endl;
+
 	string s;
 
 	while (1) {
-		cout << "Please enter a mathematical statement. (+, -, *, /, ^, () and numbers)" << endl;
+		cout << "Please enter a statement: ";
 		cin >> s;
 		try {
 			cout << "Result: " << solve_eq(s, 0, s.length()) << endl << endl;
 		}
-		catch (exception e) {
-			cout << e.what() << endl;
+		catch (calculator_exeption e) {
+			cout << e.get_message() << endl;
 		}
 	}
 }
